3f15897fedb27f8f8d8d61eebb8483344ed1375a
s-navalkar <s_navalkar@hotmail.com>
2022-11-15 11:50:02 +0530

    Hello world first commit

---
+++ Console1_For_GitJenkins/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Console1_For_GitJenkins
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! from GIT and Jenkins");
+            //Console.ReadKey();
+        }
+    }
+}
